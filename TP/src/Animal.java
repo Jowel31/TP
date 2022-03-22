7e55426abf1df212b7ac1980c450b0a5a8a0529c
PaulLoignon <p.loignon4@gmail.com>
2022-03-22 16:59:48 -0400

    l

---
+++ TP/src/Animal.java
@@ -0,0 +1,24 @@
+// Fichier :     Animal.java
+// Création:     
+// Auteurs :      
+//
+// Ce code n'est pas protégé par un copyright.
+// 
+// Historique :
+//  Créé pour le cours IFT1025 H22
+//
+
+/**
+   class Animal
+    implements the Prey/Predator relationship
+
+**/
+ 
+    // TO BE COMPLETED //
+
+class Animal implements ProiePredateur {
+    public static void main(String[] args) {
+        ProiePredateur relationPP = new ProiePredateur();
+        System.out.println(myObj.x);
+    }
+}
